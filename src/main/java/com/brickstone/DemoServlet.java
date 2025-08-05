68c2e1addaffc1e4a93bf8758738e6b67135ccde
Atul Ware <eratulware@gmail.com>
2025-08-05 09:28:11 +0530

    Pushing initial code

---
+++ src/main/java/com/brickstone/DemoServlet.java
@@ -0,0 +1,22 @@
+package com.brickstone;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
+		rd.forward(request, response);
+
+	}
+
+	
+}
